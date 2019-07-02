d49b731ddb4945985ea6b8fd366a870d7e226552
ganiaaldi <mrgad98@gmail.com>
2019-07-02 12:25:35 +0700

    SI Perpustakaan Finish

---
+++ src/p10516133/P10516133.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package p10516133;
+
+/**
+ *
+ * @author Mahasiswa
+ */
+public class P10516133 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      /**  FAnggota perpus =new FAnggota();
+        perpus.setVisible(true); */
+      /**  FBukuu buku =new FBukuu();
+        buku.setVisible(true); */
+       /**  FPeminjaman pinjam =new FPeminjaman();
+        pinjam.setVisible(true); */
+       Utama utama =new Utama();
+        utama.setVisible(true); 
+     
+
+    }
+    
+}
